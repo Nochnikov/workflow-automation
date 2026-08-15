d8a14ff2869b084f0ce1c8dae88d56f744559eff
 Nurdaulet Duisenov <nurdauletduisenov@MacBook-Air-Nurdaulet.local>
2026-08-16 02:23:52 +0500

    added fixed uow

--- app/services/auth/user_service.py
+++
@@ -1,9 +0,0 @@
-from app.services.user.base import UserServiceProtocol
-
-
-class UserService(UserServiceProtocol):
-    """*User service implementation.*"""
-
-    async def filling_user_anketa_data(self, data: dict):
-        """*Fills in the user anketa data in order to create their profile.*"""
-        pass
